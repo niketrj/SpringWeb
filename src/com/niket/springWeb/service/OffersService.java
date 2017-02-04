e0afe880d871298f2df5320d726d1eb22c867026
niketrj <niketrj@gmail.com>
2017-02-04 09:27:55 +0530

    addedd daos and service

---
+++ src/com/niket/springWeb/service/OffersService.java
@@ -0,0 +1,14 @@
+package com.niket.springWeb.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OffersService {
+
+	
+	public OffersService(){
+		System.out.println("hello World ");
+	}
+	
+	
+}
